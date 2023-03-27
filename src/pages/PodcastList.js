734955b650e4ffef31e619bf0998431201e648e7
e-hidalgo <e.hidalgo.dev@gmail.com>
2023-03-27 14:23:51 +0100

    fixed inconsistence

--- src/pages/PodcastList.js
+++ src/pages/PodcastList.js
@@ -8,7 +8,7 @@ const API_URL = 'https://api.allorigins.win/raw?url=https://itunes.apple.com/us/
 
 const PodcastList = () => {
 
-    const [podcasts, setPodcasts] = useState()
+    const [podcasts, setPodcasts] = useState(null)
     const [loading, setLoading] = useState(true);
     const [searchInput, setSearchInput] = useState('');
 
